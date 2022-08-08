3ee5aeca4405fd55a5f17bd24c4f46c8b25eaf3d
lalan992 <lalankumar992588@gmail.com>
2022-08-08 13:10:46 +0530

    problem 4

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+const lodash=require('lodash')
+const months=["january","february","March","April","May","June","July","August","September","October","November","December"];
+const oddnumber=[1,3,5,7,9,11,13,15,17,19];
+const pair=[["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
+    console.log(lodash.chunk(months,3));
+    console.log(lodash.tail(oddnumber));
+    console.log(lodash.union([2], [1, 2],[1,2,3],[1,2,3,4,5],[4,5,6,7,8,9]));
+    console.log(lodash.fromPairs(pair));
     abc.printName()
     res.send('My second ever api!')
 });
