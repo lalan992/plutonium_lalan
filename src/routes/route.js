6445921e140c6929a4502699720451f6d66b035c
lalan992 <lalankumar992588@gmail.com>
2022-08-08 21:12:31 +0530

    assignment/get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,60 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
+
+router.get('/movies', function (req, res){
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
+    res.send(movies)
+})
+router.get('/movies/:indexNumber', function (req, res){
+    const index=req.params.indexNumber;    
+    let movies = ['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins'];
+    if(index>movies.length-1){
+        res.send("Invalid index !! use a valid index")
+    }else{
+
+        res.send(movies[index])
+    }
+})
+router.get('/films', function (req, res){
+    let films = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+       
+    res.send(films)
+})
+router.get('/films/:filmId', function (req, res){
+    const index=req.params.filmId;    
+    let films = [ {
+        'id': 1,
+        'name': 'The Shining'
+       }, {
+        'id': 2,
+        'name': 'Incendies'
+       }, {
+        'id': 3,
+        'name': 'Rang de Basanti'
+       }, {
+        'id': 4,
+        'name': 'Finding Nemo'
+       }]
+    if(index>films.length || index<1){
+        res.send("No movie exists with this id")
+    }else{
+
+        res.send(films[index-1].name)
+    }
+})
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
@@ -14,6 +68,7 @@ router.get('/students', function (req, res){
     let students = ['Sabiha', 'Neha', 'Akash']
     res.send(students)
 })
+
 
 router.get('/student-details/:name', function(req, res){
     /*
