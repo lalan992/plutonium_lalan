a63a0d9203715aebd65c195b14dc2ecd96cddc45
lalan992 <lalankumar992588@gmail.com>
2022-08-12 15:37:09 +0530

    10th and 11th Aug_Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,134 +1,55 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
-
+const helper = require('../util/helper')
 const router = express.Router();
+let voters=helper.voters;
+const myplayer=helper.player;
+const booking=helper.bookings;
 
-router.get("/sol1", function (req, res) {
-    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-    ///LOGIC WILL GO HERE 
-    let arr= [1,2,3,5,6,7]
-    let missingNumber,sumOfarr,n;
-    n=arr[arr.length-1]
-    sumOfarr=arr.reduce((a,b)=>a+b)
-    missingNumber=(n*(n+1)/2)-sumOfarr;
+router.post('/players', function (req, res) {
+    //LOGIC WILL COME HERE
+    const newplayer=req.body;
+    let isPlayer=myplayer.map((x)=>x.name).includes(newplayer.name);
+    if(!isPlayer){
+        myplayer.push(newplayer)
+        res.send(  { data: myplayer , status: true }  )
+    }else{
+        res.send("This player already exists!!!")
+    }
+    
+})
 
-    res.send(  { data: missingNumber  }  );
+router.post('/players/:playerName/bookings/:bookingId', function (req, res) {
+    //LOGIC WILL COME HERE
+    const newPlayerInfo=req.body;
+    const newplayer=req.params.playerName;
+    const newBookingId=req.params.bookingId;
+    let isPlayer=myplayer.map((x)=>x.name).includes(newplayer);
+    let isBooked=booking.map((x)=>x.bookingNumber).includes(Number(newBookingId));
+    if(!isPlayer){
+        res.send("This player doesn't exists.!!!")
+    }else
+    { 
+        if(isBooked){
+        res.send("Your booking was already processed.!!!")
+        }else{
+
+            booking.push(newPlayerInfo);
+            res.send( {info:"booking confirmed", data: newPlayerInfo , status: true }  )
+        }
+    }
+    
 });
 
-router.get("/sol2", function (req, res) {
-    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
-    ///LOGIC WILL GO HERE 
-
-    let arr= [33, 34, 35, 37, 38]
-    let missingNumber,sumOfarr,n;
-    n=arr.length+1
-    sumOfarr=arr.reduce((a,b)=>a+b);
-    missingNumber=(n*(arr[0]+arr[arr.length-1])/2)-sumOfarr;
-
-    res.send(  { data: missingNumber  }  );
+router.post('/voters/:votingAge', function(req,res){
+    const newVotingAge=req.params.votingAge;
+    voters=voters.map((x)=>{if(x.age>=newVotingAge)x.votingStatus=true; else x.votingStatus=false; return x});
+    res.send({data:voters});
 });
-
-
 
 router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
+    
     res.send('My first ever api!')
 });
-
-router.get("/movies/:indexNumber", function(req, res){
-    const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
-    console.log(req.params.indexNumber)
-    let movieIndex = req.params.indexNumber
-    //check index value. less than 0 or greater than array (length - 1) are not valid
-    if(movieIndex<0 || movieIndex>=movies.length) {
-        //if the index is invalid send an error message
-        return res.send('The index value is not correct, Please check the it')
-    }
-
-    //if the index was valid send the movie at that index in response
-    let requiredMovie = movies[movieIndex]
-    res.send(requiredMovie)
-})
-
-router.get("/shoes", function(req, res){
-    let queryParams = req.query
-    let brand = queryParams.brand
-    res.send("dummy response")
-})
-
-// uses query params
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-// use path param
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
-router.get("/films", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-       //send all the films
-      res.send(films) 
-})
-
-router.get("/films/:filmId", function(req, res){
-    const films = [ {
-        "id": 1,
-        "name": "The Shining"
-       }, {
-        "id": 2,
-        "name": "Incendies"
-       }, {
-        "id": 3,
-        "name": "Rang de Basanti"
-       }, {
-        "id": 4,
-        "name": "Finding Nemo"
-       }]
-
-       let filmId = req.params.filmId
-
-       //iterate all the films
-       //search for a film whose id matches with the id recevied in request
-       for(let i = 0; i < films.length; i++){
-           let film = films[i]
-           if(film.id == filmId) {
-               //if there is a match return the response from here
-               return res.send(film)
-           }
-       }
-
-       //if there is no match give an error response
-       res.send("The film id doesn't match any movie")
-})
 
 module.exports = router;
 // adding this comment for no reason
--- src/util/helper.js
+++ src/util/helper.js
@@ -1,19 +1,66 @@
-const printDate = function() {
-    let currentDate = new Date()
-    console.log(currentDate)    
-}
+player=[
+  {
+  "name": "manish",
+  "dob": "1/1/1995",
+  "gender": "male",
+  "city": "jalandhar",
+  "sports": [
+      "swimming"
+    ]
+  },
+  {
+      "name": "gopal",
+      "dob": "1/09/1995",
+      "gender": "male",
+      "city": "delhi",
+      "sports": [
+          "soccer"
+      ]
+  },
+  {
+      "name": "lokesh",
+      "dob": "1/1/1990",
+      "gender": "male",
+      "city": "mumbai",
+      "sports": [
+          "soccer"
+      ]
+  }
 
-const printMonth = function() {
-    let currentdate = new Date()
-    let currentMonth = currentdate.getMonth() + 1
-    console.log('The current month is '+currentMonth)
-}
+  ];
 
-const getBatchInfo = function() {
-  let batchInformation = 'Radon, W3D4, the topic for today is Nodejs module system assignment discussion'
-  console.log(batchInformation)
-}
-
-module.exports.printDate = printDate
-module.exports.getCurrentMonth = printMonth
-module.exports.getCohortData = getBatchInfo
+bookings=[{
+    "bookingNumber": 1,
+    "sportId": "",
+    "centerId": "",
+    "type": "private",
+    "slot": 16286598000000,
+    "bookedOn": "31/08/2021",
+    "bookedFor": "01/09/2021"
+   }
+   ];
+voters=[
+    {
+        "name":"rohan",
+        "age":15,
+        "votingStatus":false
+    },
+    {
+        "name":"Nadeem",
+        "age":25,
+        "votingStatus":true
+    },
+    {
+        "name":"Abhay",
+        "age":45,
+        "votingStatus":true
+    },
+    {
+        "name":"Shyam",
+        "age":55,
+        "votingStatus":true
+    }
+]
+module.exports.player=player;
+module.exports.bookings=bookings;
+module.exports.voters=voters;
